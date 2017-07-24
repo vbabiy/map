f6de2e5f797c63beef5825bb868df366970e8b0e
Vitaly Babiy <vbabiy86@gmail.com>
2017-07-24 18:17:11 -0400

    Fixing spacing

--- src/services/config.ts
+++ src/services/config.ts
@@ -3,5 +3,5 @@ import { InjectionToken } from '@angular/core';
 export const NG_MAP_CONFIG_TOKEN = new InjectionToken<string>('NG_MAP_CONFIG_TOKEN');
 
 export interface ConfigOption {
-    apiUrl?: string;
+  apiUrl?: string;
 }
